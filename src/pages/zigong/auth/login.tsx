4e9c5de33372d2e6c730768c656d6048c0abeaf6
jacky.inoby <sn185672@gmail.com>
2024-03-11 11:53:24 +0800

    edit login page

--- src/pages/zigong/auth/login.tsx
+++ src/pages/zigong/auth/login.tsx
@@ -4,7 +4,9 @@ import {
   Flex,
   FormControl,
   FormLabel,
+  Heading,
   Input,
+  Link,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -12,47 +14,45 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
-  useBreakpointValue,
+  Switch,
+  Text,
+  useColorModeValue,
   useDisclosure,
   useToast,
 } from '@chakra-ui/react';
-import Form from '@components/Form';
-import LoadingLayout from '@components/Layout/LoadingLayout';
 import { ADMIN_ROUTE } from '@fixtures/constants';
-import { loginFieldConfigs } from '@fixtures/form-configs/login';
 import useAppDispatch from '@hooks/useAppDispatch';
 import useAppSelector from '@hooks/useAppSelector';
 import {
   adminForgetPasswordAsync,
   adminLoginAsync,
 } from '@reducers/admin/auth/actions';
-import type { NextPage } from 'next';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 
-const AdminAuthLoginPage: NextPage = () => {
+const Login = () => {
   const dispatch = useAppDispatch();
-  const router = useRouter();
+  const titleColor = useColorModeValue('teal.300', 'teal.200');
+  const textColor = useColorModeValue('gray.400', 'white');
   const { isOpen, onOpen, onClose } = useDisclosure();
   const toast = useToast();
-  const boxWidth = useBreakpointValue({ base: '100%', md: '25%' });
-  const imageDisplay = useBreakpointValue({ base: 'none', md: 'block' });
+  const router = useRouter();
 
   const {
     handleSubmit,
     register,
     formState: { errors },
   } = useForm();
+
   const {
     userInfo,
-    status: { loginLoading, forgetPasswordSuccess },
+    status: { loginLoading, forgetPasswordSuccess, loginSuccess },
     error: { loginError },
   } = useAppSelector((state) => state.adminAuth);
 
   useEffect(() => {
-    if (userInfo) {
+    if (userInfo || loginSuccess) {
       router.push(`/${ADMIN_ROUTE}`);
     }
   }, [userInfo, router]);
@@ -79,85 +79,174 @@ const AdminAuthLoginPage: NextPage = () => {
   };
 
   return (
-    <LoadingLayout isLoading={loginLoading}>
-      <Flex h='100vh' w='100%'>
-        <Box
-          as='main'
-          w={boxWidth}
-          h='100%'
-          display='flex'
-          flexDirection='column'
-          justifyContent='center'
-          overflow='hidden'
+    <Flex position='relative' mb='40px'>
+      <Flex
+        h={{ sm: 'initial', md: '75vh', lg: '85vh' }}
+        w='100%'
+        maxW='1044px'
+        mx='auto'
+        justifyContent='space-between'
+        mb='30px'
+        pt={{ sm: '100px', md: '0px' }}
+      >
+        <Flex
+          alignItems='center'
+          justifyContent='start'
+          style={{ userSelect: 'none' }}
+          w={{ base: '100%', md: '50%', lg: '42%' }}
         >
-          <Box as='h1'>會員登入</Box>
-          <Form fieldConfigs={loginFieldConfigs} onSubmit={handleLogin}>
-            {loginError && (
-              <section className='row__col'>
-                <p className='form__error-message'>{loginError}</p>
-              </section>
-            )}
-            <Button type='submit' w='100%' bg='cadetblue' mt='1rem'>
-              登入系統
-            </Button>
-          </Form>
-          <Button
-            onClick={onOpen}
-            color='white'
-            bg='black'
-            borderRadius={0}
-            _hover={{ bg: 'gray.800', color: 'white.500' }}
-          >
-            忘記密碼
-          </Button>
-          <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-            <ModalContent>
-              <ModalHeader>請輸入您的電子郵件</ModalHeader>
-              <ModalCloseButton />
-              <form onSubmit={handleSubmit(forgotPassword)}>
-                <ModalBody pb={6}>
-                  <FormControl>
-                    <FormLabel>電子郵件</FormLabel>
-                    <Input
-                      placeholder='電子郵件'
-                      {...register('email', {
-                        required: 'Required',
-                        pattern: {
-                          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                          message: 'invalid email address',
-                        },
-                      })}
-                    />
-                  </FormControl>
-                </ModalBody>
-                <ModalFooter>
-                  <Button colorScheme='blue' mr={3} type='submit'>
-                    提交
-                  </Button>
-                  <Button onClick={onClose}>取消</Button>
-                </ModalFooter>
-              </form>
-            </ModalContent>
-          </Modal>
-        </Box>
+          <form onSubmit={handleSubmit(handleLogin)}>
+            <Flex
+              direction='column'
+              w='100%'
+              background='transparent'
+              p='48px'
+              mt={{ md: '150px', lg: '80px' }}
+            >
+              <Heading color={titleColor} fontSize='32px' mb='10px'>
+                Welcome Back
+              </Heading>
+              <Text
+                mb='36px'
+                ms='4px'
+                color={textColor}
+                fontWeight='bold'
+                fontSize='14px'
+              >
+                Enter your email and password to sign in
+              </Text>
+              <FormControl>
+                <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>
+                  Email
+                </FormLabel>
+                <Input
+                  id='email'
+                  {...register('email', {
+                    required: 'This is required',
+                  })}
+                  borderRadius='15px'
+                  mb='24px'
+                  fontSize='sm'
+                  type='text'
+                  placeholder='Your email address'
+                  size='lg'
+                />
+                <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>
+                  Password
+                </FormLabel>
+                <Input
+                  id='password'
+                  {...register('password', {
+                    required: 'This is required',
+                  })}
+                  borderRadius='15px'
+                  mb='36px'
+                  fontSize='sm'
+                  type='password'
+                  placeholder='Your password'
+                  size='lg'
+                />
+                <FormControl display='flex' alignItems='center'>
+                  <Switch id='remember-login' colorScheme='teal' me='10px' />
+                  <FormLabel
+                    htmlFor='remember-login'
+                    mb='0'
+                    ms='1'
+                    fontWeight='normal'
+                  >
+                    Remember me
+                  </FormLabel>
+                </FormControl>
+                <Button
+                  fontSize='10px'
+                  type='submit'
+                  bg='teal.300'
+                  w='100%'
+                  h='45'
+                  mb='20px'
+                  color='white'
+                  mt='20px'
+                  _hover={{
+                    bg: 'teal.200',
+                  }}
+                  _active={{
+                    bg: 'teal.400',
+                  }}
+                >
+                  SIGN IN
+                </Button>
+              </FormControl>
+              <Flex
+                flexDirection='column'
+                justifyContent='center'
+                alignItems='center'
+                maxW='100%'
+                mt='0px'
+              >
+                <Text color={textColor} fontWeight='medium'>
+                  Don't have an account?
+                  <Link color={titleColor} as='span' ms='5px' fontWeight='bold'>
+                    Sign Up
+                  </Link>
+                </Text>
+                <Text color={textColor} fontWeight='medium' onClick={onOpen}>
+                  Forget password
+                </Text>
+              </Flex>
+            </Flex>
+          </form>
+        </Flex>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>請輸入您的電子郵件</ModalHeader>
+            <ModalCloseButton />
+            <form onSubmit={handleSubmit(forgotPassword)}>
+              <ModalBody pb={6}>
+                <FormControl>
+                  <FormLabel>電子郵件</FormLabel>
+                  <Input
+                    placeholder='電子郵件'
+                    {...register('email', {
+                      required: 'Required',
+                      pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                        message: 'invalid email address',
+                      },
+                    })}
+                  />
+                </FormControl>
+              </ModalBody>
+              <ModalFooter>
+                <Button colorScheme='blue' mr={3} type='submit'>
+                  Submit
+                </Button>
+                <Button onClick={onClose}>取消</Button>
+              </ModalFooter>
+            </form>
+          </ModalContent>
+        </Modal>
         <Box
-          as='header'
+          display={{ base: 'none', md: 'block' }}
+          overflowX='hidden'
           h='100%'
-          w='80%'
-          position='relative'
-          display={imageDisplay}
+          w='40vw'
+          position='absolute'
+          right='0px'
         >
-          <Image
-            src='/images/demo-background.png'
-            alt='Background image'
-            layout='fill'
-            objectFit='cover'
-          />
+          <Box
+            bgImage='/assets/img/signInImage.png'
+            w='100%'
+            h='100%'
+            bgSize='cover'
+            bgPosition='50%'
+            position='absolute'
+            borderBottomLeftRadius='20px'
+          ></Box>
         </Box>
       </Flex>
-    </LoadingLayout>
+    </Flex>
   );
 };
 
-export default AdminAuthLoginPage;
+export default Login;
