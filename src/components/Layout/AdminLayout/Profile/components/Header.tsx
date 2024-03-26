e9f6b88d4079733178af59e1de21abef20ed9e2c
jacky.inoby <sn185672@gmail.com>
2024-03-26 13:23:40 +0800

    edit

--- src/components/Layout/AdminLayout/Profile/components/Header.tsx
+++ src/components/Layout/AdminLayout/Profile/components/Header.tsx
@@ -3,13 +3,16 @@ import {
   Box,
   Flex,
   Text,
+  Tooltip,
   useColorModeValue,
   useToast,
 } from '@chakra-ui/react';
+import LoadingLayout from '@components/Layout/LoadingLayout';
+import MessageModal from '@components/Modal/MessageModal';
 import useAppDispatch from '@hooks/useAppDispatch';
 import useAppSelector from '@hooks/useAppSelector';
 import { adminUploadProfileImageAsync } from '@reducers/admin/auth/actions';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 interface HeaderType {
   backgroundHeader: any;
@@ -35,11 +38,18 @@ const Header = ({
   );
   const emailColor = useColorModeValue('gray.400', 'gray.300');
   const inputFileRef = useRef<HTMLInputElement>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [modalContent, setModalContent] = useState<string>('');
+  const [modalTitle, setModalTitle] = useState<string>('新增個人照片');
   const dispatch = useAppDispatch();
   const {
     userProfile,
     userInfo,
-    status: { uploadProfileImageFailed, uploadProfileImageSuccess },
+    status: {
+      uploadProfileImageFailed,
+      uploadProfileImageSuccess,
+      uploadProfileImageLoading,
+    },
     error: { uploadProfileImageError },
   } = useAppSelector((state) => state.adminAuth);
 
@@ -47,6 +57,10 @@ const Header = ({
 
   const handleAvatarClick = () => {
     inputFileRef.current?.click();
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
   };
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -57,123 +71,136 @@ const Header = ({
 
     const formData = new FormData();
     formData.append('profileImage', file);
-
     dispatch(adminUploadProfileImageAsync({ formData, userId: userInfo?.id }));
   };
 
   useEffect(() => {
     if (uploadProfileImageSuccess) {
-      toast({
-        title: '上傳相片成功成功',
-        status: 'success',
-        isClosable: true,
-      });
-    } else if (uploadProfileImageFailed) {
-      toast({
-        title: '上傳相片失敗',
-        description: uploadProfileImageError || '未知錯誤。',
-        status: 'error',
-        isClosable: true,
-      });
+      setIsModalOpen(true);
+      setModalContent('新增圖片成功！');
+    }
+
+    if (uploadProfileImageFailed) {
+      setIsModalOpen(true);
+      setModalContent('');
     }
   }, [uploadProfileImageFailed, uploadProfileImageSuccess]);
 
+  useEffect(() => {
+    setIsModalOpen(false);
+  }, []);
+
   return (
-    <Box
-      mb={{ sm: '205px', md: '75px', xl: '70px' }}
-      borderRadius='15px'
-      px='0px'
-      display='flex'
-      flexDirection='column'
-      justifyContent='center'
-      alignItems='center'
-    >
+    <LoadingLayout isLoading={uploadProfileImageLoading}>
       <Box
-        bgImage={backgroundHeader}
-        w='100%'
-        h='300px'
-        borderRadius='25px'
-        bgPosition='50%'
-        bgRepeat='no-repeat'
-        position='relative'
+        mb={{ sm: '205px', md: '75px', xl: '70px' }}
+        borderRadius='15px'
+        px='0px'
         display='flex'
+        flexDirection='column'
         justifyContent='center'
+        alignItems='center'
       >
-        <Flex
-          direction={{ sm: 'column', md: 'row' }}
-          mx='1.5rem'
-          maxH='330px'
-          w={{ sm: '90%', xl: '95%' }}
-          justifyContent={{ sm: 'center', md: 'space-between' }}
-          align='center'
-          backdropFilter='saturate(200%) blur(50px)'
-          position='absolute'
-          boxShadow='0px 2px 5.5px rgba(0, 0, 0, 0.02)'
-          border='2px solid'
-          borderColor={borderProfileColor}
-          bg={backgroundProfile}
-          p='24px'
-          borderRadius='20px'
-          transform={{
-            sm: 'translateY(45%)',
-            md: 'translateY(110%)',
-            lg: 'translateY(160%)',
-          }}
+        <Box
+          bgImage={backgroundHeader}
+          w='100%'
+          h='300px'
+          borderRadius='25px'
+          bgPosition='50%'
+          bgRepeat='no-repeat'
+          position='relative'
+          display='flex'
+          justifyContent='center'
         >
           <Flex
-            align='center'
-            mb={{ sm: '10px', md: '0px' }}
             direction={{ sm: 'column', md: 'row' }}
-            w={{ sm: '100%' }}
-            textAlign={{ sm: 'center', md: 'start' }}
+            mx='1.5rem'
+            maxH='330px'
+            w={{ sm: '90%', xl: '95%' }}
+            justifyContent={{ sm: 'center', md: 'space-between' }}
+            align='center'
+            backdropFilter='saturate(200%) blur(50px)'
+            position='absolute'
+            boxShadow='0px 2px 5.5px rgba(0, 0, 0, 0.02)'
+            border='2px solid'
+            borderColor={borderProfileColor}
+            bg={backgroundProfile}
+            p='24px'
+            borderRadius='20px'
+            transform={{
+              sm: 'translateY(45%)',
+              md: 'translateY(110%)',
+              lg: 'translateY(160%)',
+            }}
           >
-            <input
-              type='file'
-              ref={inputFileRef}
-              onChange={handleFileChange}
-              style={{ display: 'none' }}
-              accept='image/*'
-            />
-            {userProfile?.profileImage ? (
-              <Avatar
-                me={{ md: '22px' }}
-                src={userProfile.profileImage.imageUrl}
-                w='80px'
-                h='80px'
-                borderRadius='15px'
-                onClick={handleAvatarClick}
-                cursor='pointer'
+            <Flex
+              align='center'
+              mb={{ sm: '10px', md: '0px' }}
+              direction={{ sm: 'column', md: 'row' }}
+              w={{ sm: '100%' }}
+              textAlign={{ sm: 'center', md: 'start' }}
+            >
+              <input
+                type='file'
+                ref={inputFileRef}
+                onChange={handleFileChange}
+                style={{ display: 'none' }}
+                accept='image/*'
               />
-            ) : (
-              <Avatar
-                me={{ md: '22px' }}
-                src={avatarImage}
-                w='80px'
-                h='80px'
-                borderRadius='15px'
-              />
-            )}
-            <Flex direction='column' maxWidth='100%' my={{ sm: '14px' }}>
-              <Text
-                fontSize={{ sm: 'lg', lg: 'xl' }}
-                color={textColor}
-                fontWeight='bold'
-                ms={{ sm: '8px', md: '0px' }}
-              >
-                {name}
-              </Text>
-              <Text
-                fontSize={{ sm: 'sm', md: 'md' }}
-                color={textColor}
-                fontWeight='semibold'
-              >
-                {email}
-              </Text>
+              {userProfile?.profileImage ? (
+                <Tooltip label='點擊替換圖片' hasArrow>
+                  <Avatar
+                    me={{ md: '22px' }}
+                    src={userProfile.profileImage.imageUrl}
+                    w='80px'
+                    h='80px'
+                    borderRadius='15px'
+                    onClick={handleAvatarClick}
+                    cursor='pointer'
+                  />
+                </Tooltip>
+              ) : (
+                <Tooltip label='點擊替換圖片' hasArrow>
+                  <Avatar
+                    me={{ md: '22px' }}
+                    src={avatarImage}
+                    w='80px'
+                    h='80px'
+                    borderRadius='15px'
+                    onClick={handleAvatarClick}
+                  />
+                </Tooltip>
+              )}
+              <Flex direction='column' maxWidth='100%' my={{ sm: '14px' }}>
+                <Text
+                  fontSize={{ sm: 'lg', lg: 'xl' }}
+                  color={textColor}
+                  fontWeight='bold'
+                  ms={{ sm: '8px', md: '0px' }}
+                >
+                  {name}
+                </Text>
+                <Text
+                  fontSize={{ sm: 'sm', md: 'md' }}
+                  color={textColor}
+                  fontWeight='semibold'
+                >
+                  {email}
+                </Text>
+              </Flex>
             </Flex>
           </Flex>
-        </Flex>
+        </Box>
+        <MessageModal
+          title={modalTitle}
+          isActive={isModalOpen}
+          error={uploadProfileImageError}
+          onClose={handleCloseModal}
+        >
+          {modalContent}
+        </MessageModal>
       </Box>
-    </Box>
+    </LoadingLayout>
   );
 };
 
