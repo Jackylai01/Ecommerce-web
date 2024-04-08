546e230edddc077d299caf23c18ac29a730180fd
jacky.inoby <sn185672@gmail.com>
2024-04-08 17:56:43 +0800

    edit

--- src/components/CustomPage/NestedDisplayUI/src/SelectableImage.tsx
+++ src/components/CustomPage/NestedDisplayUI/src/SelectableImage.tsx
@@ -1,3 +1,12 @@
+import {
+  Alert,
+  AlertIcon,
+  Box,
+  Image,
+  Input,
+  Progress,
+  Text,
+} from '@chakra-ui/react';
 import generateUUID from '@helpers/generate-uuid';
 import useAppDispatch from '@hooks/useAppDispatch';
 import useAppSelector from '@hooks/useAppSelector';
@@ -11,14 +20,16 @@ const SelectableImage = ({
   tempProductId,
 }: ElementProps & { tempProductId?: string }) => {
   const dispatch = useAppDispatch();
-  const fileInputRef = useRef<HTMLInputElement>(null);
+  const fileInputRef = useRef<any>(null);
 
   const [imageId, setImageId] = useState<string>(() => generateUUID());
   const [src, setSrc] = useState<any>(element.src);
 
-  const uploadedImages = useAppSelector(
-    (state) => state.adminUpload.uploadedImages,
-  );
+  const {
+    uploadedImages,
+    status: { uploadLoading },
+    error: { uploadError },
+  } = useAppSelector((state) => state.adminUpload);
 
   useEffect(() => {
     const matchingImage = uploadedImages.find(
@@ -34,28 +45,50 @@ const SelectableImage = ({
   ) => {
     const file = event.target.files ? event.target.files[0] : null;
     if (file) {
-      // 上传图片时，连同唯一标识符一起传递
       const action = await dispatch(
         adminUploadAsync({ file, tempProductId, imageId }),
       ).unwrap();
       setSrc(action.imageUrl);
-
       setImageId(action.imageId);
     }
   };
 
   return (
-    <div onClick={() => isEdit && fileInputRef.current?.click()}>
-      <img src={src} alt='Selectable' />
-      {isEdit && (
-        <input
-          type='file'
-          ref={fileInputRef}
-          style={{ display: 'none' }}
-          onChange={handleImageChange}
-        />
+    <Box position='relative' textAlign='center'>
+      {uploadLoading && <Progress size='xs' isIndeterminate />}
+      {uploadError && (
+        <Alert status='error'>
+          <AlertIcon />
+          {uploadError}
+        </Alert>
       )}
-    </div>
+      <Image src={src} alt='Selectable' maxWidth='100%' />
+      {isEdit && (
+        <>
+          <Input
+            type='file'
+            onChange={handleImageChange}
+            size='sm'
+            accept='image/*'
+            display='none'
+            ref={fileInputRef}
+          />
+          <Text
+            as='button'
+            onClick={() => fileInputRef.current.click()}
+            position='absolute'
+            top='50%'
+            left='50%'
+            transform='translate(-50%, -50%)'
+            bgColor='rgba(255, 255, 255, 0.6)'
+            p={2}
+            borderRadius='md'
+          >
+            Click to change image
+          </Text>
+        </>
+      )}
+    </Box>
   );
 };
 
