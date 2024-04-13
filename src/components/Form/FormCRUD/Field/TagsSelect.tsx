6128e6160a1afecf3f3f962f7c902e581c1337c5
jacky.inoby <sn185672@gmail.com>
2024-04-13 22:33:57 +0800

    edit

--- src/components/Form/FormCRUD/Field/TagsSelect.tsx
+++ src/components/Form/FormCRUD/Field/TagsSelect.tsx
@@ -35,7 +35,7 @@ export const TagsMultiSelect = ({
 
   useEffect(() => {
     setValue(name, selectedTags);
-  }, [selectedTags, setValue, name]);
+  }, []);
 
   return (
     <FormControl isRequired={isRequired}>
