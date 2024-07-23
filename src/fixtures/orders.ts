8120b8f70c8e72e89381ea47927f8c7cef9a5163
JackyBusiness <ns1856769@gmail.com>
2024-07-23 11:57:13 +0800

    edit

--- src/fixtures/orders.ts
+++ src/fixtures/orders.ts
@@ -2,8 +2,21 @@ export const getOrderStatusInChinese = (order: any) => {
   const latestPaymentStatus = order.payments?.[0]?.PaymentStatus;
   const latestShipmentStatus = order.shipments?.[0]?.shipmentStatus;
 
-  if (latestPaymentStatus === 'Failed' || order.status === 'Cancelled') {
+  if (order.status === 'Cancelled') {
     return '已取消';
+  } else if (order.status === 'Shipped') {
+    return '已出貨';
+  } else if (order.status === 'Completed') {
+    return '已完成';
+  } else if (order.status === 'Pending') {
+    return '待處理';
+  } else if (order.paymentMethod === 'COD' && order.status === 'COD_Pending') {
+    return '貨到付款待處理';
+  } else if (
+    latestPaymentStatus === 'Failed' ||
+    latestShipmentStatus === 'Returned'
+  ) {
+    return '已退貨';
   } else if (
     latestPaymentStatus === 'Refunded' ||
     latestShipmentStatus === 'Returned'
@@ -30,8 +43,6 @@ export const getOrderStatusInChinese = (order: any) => {
     latestShipmentStatus === 'Pending'
   ) {
     return '待處理';
-  } else if (order.paymentMethod === 'COD' && order.status === 'COD_Pending') {
-    return '貨到付款待處理';
   } else {
     return '待處理';
   }
