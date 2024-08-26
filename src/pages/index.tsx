387e3d72e86b89ab3670c2b6d3a44de03464bf64
JackyBusiness <ns1856769@gmail.com>
2024-08-26 17:04:57 +0800

    edit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,65 @@
-import { Box } from '@chakra-ui/react';
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Box,
+  Button,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { Footer } from '@components/Footer';
 import { Banner } from '@components/Home/Banner';
 import { FeaturedProducts } from '@components/Home/FeaturedProducts';
 import { TopCategories } from '@components/Home/TopCategories';
 import { fakeProducts } from '@helpers/products';
 import type { NextPage } from 'next';
+import { useEffect, useRef } from 'react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 const HomePage: NextPage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef(null);
+
+  // 當進入頁面時自動打開彈跳視窗
+  useEffect(() => {
+    onOpen();
+  }, [onOpen]);
+
   return (
     <Box>
+      {/* 通知彈跳視窗 */}
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+              通知
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              嗨這裡是測試用的網站，後台連結如下:
+              https://ecommerce-web-tawny.vercel.app/zigong
+              <Box>測式帳號:sn185672@gmail.com</Box>
+              <Box>測式密碼:12345678</Box>
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                關閉
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+
+      {/* 首頁內容 */}
       <Banner />
       <TopCategories />
       <FeaturedProducts title='Best Deals For You' products={fakeProducts} />
