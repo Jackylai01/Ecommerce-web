34c1162ac237a633dead0d4c0db74e28a287f08a
Jackylai01 <sn185672@gmail.com>
2024-08-31 21:20:34 +0800

    fix(news): add ssr setting

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,15 +1,42 @@
 import { Box } from '@chakra-ui/react';
-import { NextPage } from 'next';
-import dynamic from 'next/dynamic';
+import NewsTabs from '@components/News';
 
-const News = dynamic(() => import('@components/News'), { ssr: false });
+import { NewsItem } from '@models/responses/news';
+import {
+  apiGetAllPublicNews,
+  apiGetAllPublicNewsCategories,
+} from '@services/public/news/news';
+import { GetServerSideProps, NextPage } from 'next';
 
-const NewsPages: NextPage = () => {
+interface NewsPagesProps {
+  newsList: NewsItem[];
+  categories: any[];
+}
+
+const NewsPages: NextPage<NewsPagesProps> = ({ newsList, categories }) => {
   return (
     <Box>
-      <News />
+      <NewsTabs newsList={newsList} categories={categories} />
     </Box>
   );
 };
 
+export const getServerSideProps: GetServerSideProps = async () => {
+  try {
+    const newsResponse = await apiGetAllPublicNews({ page: 1, limit: 10 });
+    const categoriesResponse = await apiGetAllPublicNewsCategories();
+
+    return {
+      props: {
+        newsList: newsResponse.result.data,
+        categories: categoriesResponse.result.data || [],
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching news data:', error);
+    return {
+      notFound: true,
+    };
+  }
+};
 export default NewsPages;
