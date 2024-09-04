1fc4cb099e280d450e6f39b2e9930de8176cbc5a
Jackylai01 <sn185672@gmail.com>
2024-09-04 13:45:11 +0800

    edit

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,18 +1,7 @@
-import { Box, Flex } from '@chakra-ui/react';
-import Chat from '@components/chat';
-import ChatList from '@components/ClientChat';
+import { Flex } from '@chakra-ui/react';
 
 const ChatApp = () => {
-  return (
-    <Flex w='100%' h='100vh' bg='gray.900'>
-      <Box w='30%' h='100%' bg='gray.800'>
-        <ChatList />
-      </Box>
-      <Box w='70%' h='100%'>
-        <Chat />
-      </Box>
-    </Flex>
-  );
+  return <Flex w='100%' h='100vh' bg='gray.900'></Flex>;
 };
 
 export default ChatApp;
