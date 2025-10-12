1396d6b9e77f7c3679115eb2d5f9eed04881940c
Jackylai01 <sn185672@gmail.com>
2025-10-12 13:11:18 +0800

    fix(home): remove modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,67 +1,17 @@
-import {
-  AlertDialog,
-  AlertDialogBody,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogOverlay,
-  Box,
-  Button,
-  Image,
-  Link,
-  useDisclosure,
-} from '@chakra-ui/react';
+import { Box, Image, Link } from '@chakra-ui/react';
 import Carousel from '@components/Carouse';
 import { Footer } from '@components/Footer';
 import { Banner } from '@components/Home/Banner';
 import { TopCategories } from '@components/Home/TopCategories';
 import { images, size } from '@fixtures/Carousel';
 import type { NextPage } from 'next';
-import { useEffect, useRef } from 'react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 const HomePage: NextPage = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const cancelRef = useRef(null);
-
-  useEffect(() => {
-    onOpen();
-  }, [onOpen]);
-
   return (
     <Box>
-      <AlertDialog
-        isOpen={isOpen}
-        leastDestructiveRef={cancelRef}
-        onClose={onClose}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
-              通知
-            </AlertDialogHeader>
-
-            <AlertDialogBody>
-              <h1> 嗨這裡是測試用的網站，後台連結如下:</h1>
-              <Button>
-                <Link href='/zigong'>前往管理員後台</Link>
-              </Button>
-              <Button>
-                <Link href='/public/auth/login'>前往個人會員後台</Link>
-              </Button>
-              <Box>如果網頁有Loading 的狀況請稍等30秒左右</Box>
-            </AlertDialogBody>
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onClose}>
-                關閉
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
-
       <Banner />
       <Carousel images={images} size={size} />
       <TopCategories />
